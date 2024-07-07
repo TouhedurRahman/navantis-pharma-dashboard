26fa4da9435fd818470e088307b1b5753d33f294
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-07 10:32:28 +0600

    loader component added

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <div className="dot-spinner">
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
